0ac5d1eab594df18e8fac58f44515742b9a302dd
Daniele Moser <dnlmsr0@gmail.com>
2022-07-05 13:55:24 +0200

    Created simple struct with id and temperatures

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use reqwest;
 use serde_json;
 
+#[derive(Debug)]
+struct Forecast {
+    id: u64,
+    temperature_max: i64,
+    temperature_min: i64,
+}
+
 fn get_weather_data() -> Result<String, reqwest::Error> {
     let body = reqwest::blocking::get("https://www.meteotrentino.it/protcivtn-meteo/api/front/previsioneOpenDataLocalita?localita=TRENTO")?
         .text()?;
@@ -12,5 +19,11 @@ fn deserialize_json(data: String) -> serde_json::Result<serde_json::Value> {
 }
 
 fn main() {
-    println!("{:?}",deserialize_json(get_weather_data().unwrap()).unwrap());
+    let data = deserialize_json(get_weather_data().unwrap()).unwrap();
+    let forecast = Forecast{
+        id: data["idPrevisione"].as_u64().unwrap(),
+        temperature_max: data["previsione"][0]["giorni"][0]["tMaxGiorno"].as_i64().unwrap(),
+        temperature_min: data["previsione"][0]["giorni"][0]["tMinGiorno"].as_i64().unwrap(),
+    };
+    println!("{:?}",forecast);
 }

807596145d5f5f83abe71194ff3ec86bcd2a9aca
Daniele Moser <dnlmsr0@gmail.com>
2022-07-05 14:19:12 +0200

    Passing argument to URL

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,9 @@ struct Forecast {
     temperature_min: i64,
 }
 
-fn get_weather_data() -> Result<String, reqwest::Error> {
-    let body = reqwest::blocking::get("https://www.meteotrentino.it/protcivtn-meteo/api/front/previsioneOpenDataLocalita?localita=TRENTO")?
+fn get_weather_data(locality: &String) -> Result<String, reqwest::Error> {
+    let base_url = String::from("https://www.meteotrentino.it/protcivtn-meteo/api/front/previsioneOpenDataLocalita?localita=");
+    let body = reqwest::blocking::get(base_url+locality)?
         .text()?;
     Ok(body)
 }
@@ -29,13 +30,14 @@ fn deserialize_json(data: String) -> serde_json::Result<serde_json::Value> {
 }
 
 fn main() {
-    let data = deserialize_json(get_weather_data().unwrap()).unwrap();
+    let args = Args::parse();
+
+    let data = deserialize_json(get_weather_data(&args.locality).unwrap()).unwrap();
     let forecast = Forecast{
         id: data["idPrevisione"].as_u64().unwrap(),
         temperature_max: data["previsione"][0]["giorni"][0]["tMaxGiorno"].as_i64().unwrap(),
         temperature_min: data["previsione"][0]["giorni"][0]["tMinGiorno"].as_i64().unwrap(),
     };
 
-    let args = Args::parse();
-    println!("{:?}",args);
+    println!("{:?}",forecast);
 }

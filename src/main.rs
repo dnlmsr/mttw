7f898f02bd95843c32ac2837b26e7ca7db7472f2
Daniele Moser <dnlmsr0@gmail.com>
2022-07-05 13:28:41 +0200

    Moved deserialize in function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use reqwest;
 use serde_json;
 
-fn get_weather_data() -> Result<serde_json::Value, reqwest::Error> {
+fn get_weather_data() -> Result<String, reqwest::Error> {
     let body = reqwest::blocking::get("https://www.meteotrentino.it/protcivtn-meteo/api/front/previsioneOpenDataLocalita?localita=TRENTO")?
         .text()?;
-    let deserialized: serde_json::Value = serde_json::from_str(&body).unwrap();
-    Ok(deserialized)
+    Ok(body)
+}
+
+fn deserialize_json(data: String) -> serde_json::Result<serde_json::Value> {
+   serde_json::from_str(&data)
 }
 
 fn main() {
-    println!("{}",get_weather_data().unwrap());
+    println!("{:?}",deserialize_json(get_weather_data().unwrap()).unwrap());
 }

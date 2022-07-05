7a17621b36eee3ac09cc5a7fd9a448d4005542c1
Daniele Moser <dnlmsr0@gmail.com>
2022-07-05 08:04:30 +0200

    Simple curl with curl library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use curl::easy::Easy;
+use std::io::{stdout, Write};
+
 fn main() {
-    println!("mttw!");
+
+    let mut easy = Easy::new();
+    easy.url("https://www.meteotrentino.it/protcivtn-meteo/api/front/previsioneOpenDataLocalita?localita=TRENTO").unwrap();
+    easy.write_function(|data| {
+        stdout().write_all(data).unwrap();
+        Ok(data.len())
+    }).unwrap();
+    easy.perform().unwrap();
+    println!("{}",easy.response_code().unwrap());
 }
